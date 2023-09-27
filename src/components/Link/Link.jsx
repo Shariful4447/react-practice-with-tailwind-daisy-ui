65cb28302922c824f51f70d14b3feef11f612462
Shariful4447 <shariful.islam4447@gmail.com>
2023-09-27 05:23:08 +0200

    created custom navbar with tailwind

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+
+const Link = ({route}) => {
+    return (
+
+
+            <li className="mr-12">
+                <a href={route.path}>{route.name}</a>
+            </li>
+        
+    );
+};
+
+export default Link;
